9203f422d0365fa06881cc82773ef23ffe60d0e9
massagist2010 <rusy_boob2@mail.ru>
2022-09-26 17:26:39 +0300

    цикл 1 д.з 1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // ЦИКЛ ЧАСТЬ 1/ Задание 1
+        System.out.println("ЦИКЛ ЧАСТЬ 1/ Задание 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(+i);
+        }
+        // sleduechee.
+        System.out.println("sleduechee");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(+i);
+        }
+        // dalshe
+        System.out.println("dalshe");
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(+i);
+        }
+        // chetvertoe:
+        System.out.println("chetvertoe:");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(+i);}
+        }
+    }
